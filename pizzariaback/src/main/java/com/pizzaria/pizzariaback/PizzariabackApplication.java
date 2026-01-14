34e49289a334ef1d2e017b22b3dc1d37fd6fbbdd
ItaloHespanholPeres <pereshespanhol1@gmail.com>
2026-01-14 17:45:47 -0300

    Projeto inicial da pizzaria (frontend + backend)

---
+++ pizzariaback/src/main/java/com/pizzaria/pizzariaback/PizzariabackApplication.java
@@ -0,0 +1,13 @@
+package com.pizzaria.pizzariaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariabackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariabackApplication.class, args);
+	}
+
+}
---
+++ pizzariaback/src/test/java/com/pizzaria/pizzariaback/PizzariabackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzaria.pizzariaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariabackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
