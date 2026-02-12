5bb9a54f713cb71cd83c3e95a03ff1c9de04beaa
ItaloHespanholPeres <pereshespanhol1@gmail.com>
2026-02-12 02:08:26 -0300

    Mudancas no controller e service para usar as classes de DTO

--- pizzariaback/src/main/java/com/pizzaria/pizzariaback/controller/PizzaController.java
+++ pizzariaback/src/main/java/com/pizzaria/pizzariaback/controller/PizzaController.java
@@ -1,13 +1,18 @@
 package com.pizzaria.pizzariaback.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pizzaria.pizzariaback.Entities.Pizza;
+import com.pizzaria.pizzariaback.dto.PizzaRequestDTO;
+import com.pizzaria.pizzariaback.dto.PizzaResponseDTO;
 import com.pizzaria.pizzariaback.service.PizzaService;
 
 @RestController
@@ -17,9 +22,18 @@ public class PizzaController {
     @Autowired
     private PizzaService pizzaService;
 
-    @GetMapping("/all")
-    public List<Pizza> ListAllPizza(){
-        return pizzaService.ListAllPizza();
+    @GetMapping
+    public Page<PizzaResponseDTO> listAllPizza(Pageable pageable){
+        return pizzaService.listAllPizza(pageable);
     } 
+
+    
+    @PostMapping
+    public ResponseEntity<PizzaResponseDTO> createPizza(@RequestBody PizzaRequestDTO pizzaToSave){
+        PizzaResponseDTO savedPizza = pizzaService.save(pizzaToSave);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(savedPizza);
+    }
 
 }
--- pizzariaback/src/main/java/com/pizzaria/pizzariaback/service/PizzaService.java
+++ pizzariaback/src/main/java/com/pizzaria/pizzariaback/service/PizzaService.java
@@ -1,11 +1,14 @@
 package com.pizzaria.pizzariaback.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.pizzaria.pizzariaback.Entities.Pizza;
+import com.pizzaria.pizzariaback.dto.PizzaRequestDTO;
+import com.pizzaria.pizzariaback.dto.PizzaResponseDTO;
+import com.pizzaria.pizzariaback.mappers.PizzaMapper;
 import com.pizzaria.pizzariaback.repository.PizzaRepository;
 
 @Service
@@ -14,7 +17,16 @@ public class PizzaService {
     @Autowired
     private PizzaRepository pizzaRepository;
 
-    public List<Pizza> ListAllPizza(){
-        return pizzaRepository.findAll();
+    public Page<PizzaResponseDTO> listAllPizza(Pageable pageable){
+        return pizzaRepository
+                .findAll(pageable)
+                .map(PizzaResponseDTO::new);
+    }
+
+    public PizzaResponseDTO save(PizzaRequestDTO dto){
+        Pizza pizzaToSave = PizzaMapper.toEntity(dto);
+        Pizza savedPizza = pizzaRepository.save(pizzaToSave);
+
+        return PizzaMapper.toDto(savedPizza);
     }
 }
