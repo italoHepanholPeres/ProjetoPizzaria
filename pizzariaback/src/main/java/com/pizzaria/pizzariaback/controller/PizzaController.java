043020f43346842e3e1ae556eb707d050954d708
ItaloHespanholPeres <pereshespanhol1@gmail.com>
2026-04-23 14:08:06 -0300

    Deletar pizza pelo ID

--- pizzariaback/src/main/java/com/pizzaria/pizzariaback/controller/PizzaController.java
+++ pizzariaback/src/main/java/com/pizzaria/pizzariaback/controller/PizzaController.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +36,12 @@ public class PizzaController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(savedPizza);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePizzaById(@PathVariable Long id){
+        pizzaService.deleteByID(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
